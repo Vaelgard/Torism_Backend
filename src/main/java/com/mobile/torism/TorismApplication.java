2127561de28e54983caacd79fe80f5ead64ca8cc
ymeriaf5 <younessmeriaf3@gmail.com>
2024-12-17 09:55:56 +0100

    first commit

---
+++ src/main/java/com/mobile/torism/TorismApplication.java
@@ -0,0 +1,13 @@
+package com.mobile.torism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TorismApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TorismApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mobile/torism/TorismApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mobile.torism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TorismApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
